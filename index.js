fe3626114f8d8f775f86e77a493c6db94bd8567c
patostickar <pstickar@gmail.com>
2022-04-20 10:26:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+  const readmeTemplate = (
+    await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+  ).toString("utf-8");
+
+  const quotes = await (await fetch("https://type.fit/api/quotes")).json();
+
+  const id = Math.floor(Math.random() * 1643);
+  const quote = quotes[id];
+
+  const readme = readmeTemplate
+    .replace("{id}", id)
+    .replace("{quote}", quote.text)
+    .replace("{character}", quote.author);
+
+  await fs.writeFile("README.md", readme);
+}
+
+main();
